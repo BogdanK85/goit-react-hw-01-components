e26f0c33022ff9311586e94447d02760e3b5cc45
BogdanK85 <kobakbogdan@gmail.com>
2023-09-01 02:10:58 +0300

    add 1 & 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import user from './Json/user.json';
+import { Profile } from '../components/Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import data from './Json/data.json'
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main className='main'>
+      <h1 className='h1'>React homework 1</h1>
+
+      <section className='section-profile'>
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </section>
+
+      <section className='sectoin-statistics'>
+        <Statistics title='Upload stats' stats={data} />
+      </section>
+
+    </main>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// const div = React.createElement(
+//   'div',
+//   {
+//     name: 'main',
+//     id: 'test-id',
+//     className: 'test-class',
+//     children: ['hello', ' ','from', ' UA']
+//   },
+  
+// )
+
+// console.log('div', div);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+
